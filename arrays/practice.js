9f6b7b8d37d9f0bebdfdfffd79f571da7226f5a9
Ryan Leavitt <hopelessgains@gmail.com>
2021-03-24 14:05:06 -0600

    finished lab

--- arrays/practice.js
+++ arrays/practice.js
@@ -18,6 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
+function first (arr){
+ return arr [0]
+}
 
 
 
@@ -33,6 +36,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
+function last(arr){
+  return arr[2]
+}
 
 
 
@@ -48,8 +54,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
-
+function looper(family){
+for(let i = 0; i <= family.length - 1; i++){
+  alert(family[i])
+ }
+ }
 
 ////////// PROBLEM 4 //////////
 
@@ -63,7 +72,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters){
+  for(let i= letters.length-1; i>=0; i--){
+    alert(letters[i])
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,7 +91,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+function evenFinder(nums){
+  const equalNums = []
+  for(let i = 0; i < nums.length; i++){
+    if (nums [i] % 2 === 0){
+      equalNums.push (nums[i]);
+    }
+  } return equalNums
 
+}
 
 
 
@@ -109,6 +130,23 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 //Code Here
 
 
+ 
+  //       even.push(numbersArray[i]) } 
+  //              // number is even
+  //       else {
+  //       // number is not even (=odd)
+  //       odd.push(numbersArray[i]) }
+  //   }
+  
+  // return (odd[i],even[i])
+  // } 
+    // create an object with the odd and even array in it
+    // const numbersArray= {
+      // array1: odd,
+      // array2: even
+    // };
+  
+    
 
 ////////// PROBLEM 7 //////////
 
